2970d216b58db2bdc03b1998daf2252d0aa2af50
Akhil MISHRA <156499973+AKHIL47MISHRA@users.noreply.github.com>
2024-11-28 17:39:31 +0530

    Update peom.py

--- peom.py
+++ peom.py
@@ -1,2 +1,4 @@
+# this is the example of triple string which is use t write long text in python like peom and speech etc
+
 print(''' twinkle twinkle little star\n
       how i wonder what you are\n up avobe the world so high like  a diamond in the sky''')
