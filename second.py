52a4bae153539c2941405c020110ccf046a54f17
Akhil MISHRA <156499973+AKHIL47MISHRA@users.noreply.github.com>
2024-11-28 17:43:08 +0530

    Update second.py

--- second.py
+++ second.py
@@ -1,5 +1,9 @@
+# THIS CODE TELLS ABOUT we can import  package in python for using them in  our code  import means( already written code by anybody
+) , second thing it import  a funny pacakage pytsx3 as you can se what he  does .
+
 import pyttsx3
 jarvis = pyttsx3.init()
-jarvis.say("hello i am jarvis i and i am just a module which anybody can install throw giving command pip install jarvis and i wanna say one thing to you ")
+jarvis.say("hello i am jarvis i and i am just a module which anybody can install throw giving command pip install jarvis and i
+wanna say one thing to you FUCK 📴 ")
 
 jarvis.runAndWait()
